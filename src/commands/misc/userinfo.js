6523324fc7d2f34a2ed8b2a8eeb6b8ca8cba718a
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-09-14 13:45:31 +0200

    Add UserInfoCommand

---
+++ src/commands/misc/userinfo.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord-akairo');
+
+class UserInfoCommand extends Command {
+    constructor() {
+        super('userinfo', {
+           aliases: ['userinfo', 'info'],
+        //    description: "Affiche les informations de l'utilisateur",
+        //    ignoreCooldown: '263328052237434880',
+        //    ignorePermissions: '263328052237434880',
+           userPermissions: 'KICK_MEMBERS',
+           clientPermissions: 'KICK_MEMBERS',
+        //    ratelimit: 1,
+           cooldown: 5000,
+           typing: true,
+           ownerOnly: true,
+           channel: 'guild',
+           args: [
+               { id: 'member', type: 'member', default: message => message.member },
+           ],
+        });
+    }
+
+    exec(message, args) {
+        return message.channel.send({ embeds: [
+            this.client.functions.embed()
+            .setTitle(`${args.member.displayName} (${args.member.id})`)
+            .setThumbnail(args.member.user.displayAvatarURL())
+            .setDescription(`Son compte a été créé le ${args.member.user.createdAt}`)
+        ]})
+    }
+}
+
+module.exports = UserInfoCommand;

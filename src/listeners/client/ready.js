9f89e65f9be57926a5d5c7ef671befdc2d99ea88
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-10-22 21:20:25 +0200

    setPresence (ready)

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -1,15 +1,20 @@
-const { Listener } = require('discord-akairo');
+const { Listener } = require('discord-akairo')
 
 class ReadyListener extends Listener {
     constructor() {
         super('ready', {
             emitter: 'client',
             event: 'ready'
-        });
+        })
     }
 
     exec() {
-        console.log('I\'m ready!');
+        this.client.user.setPresence({ activities: [{ name: 'with Lindow', type: 'PLAYING' }], status: 'dnd' })
+
+        const notBotUsers = this.client.users.cache.filter(user => !user.bot)
+        const textChannel = this.client.channels.cache.filter(channel => channel.type == "GUILD_TEXT")
+
+        console.log(`Prêt à servir ${this.client.guilds.cache.size} serveurs, ${notBotUsers.size} utilisateurs (bot non-inclus) et ${textChannel.size} salons textuels.`)
     }
 }
 
